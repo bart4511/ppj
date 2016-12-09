e12317cab83e2c3c90a2cdae44db5d8a006d5676
Bartosz Rogowski <rogowskibart@gmail.com>
2016-12-09 05:56:30 +0100

    Rozwiazane zadanie VIII

--- ppj08/Ppj08.java
+++ ppj08/Ppj08.java
@@ -8,6 +8,21 @@ class Ppj08 {
         x -= 1.0;
         System.out.println("double = " + x);
     }
+
+    public static void CzyPalindrom(char[] tab) {
+        boolean czyPalindrom = true;
+        for(int i = 0; i < tab.length / 2; i++) {
+            if(tab[i] != tab[tab.length - 1 - i]) {
+                czyPalindrom = false;
+            }
+        }
+        if(czyPalindrom) {
+            System.out.println("Tablica jest palindromem.");
+        } else {
+            System.out.println("Tablica nie jest palindromem.");
+        }
+    }
+
     public static void main(String[] args) {
         // Zadanie I
         System.out.println("\nZadanie I\n");
@@ -50,6 +65,11 @@ class Ppj08 {
 
         // Zadanie VIII
         System.out.println("\nZadanie VIII\n");
+        char[] palindrom = {1, 5, 7, 4, 23, 23, 4, 7, 5, 1};//new char[];
+        char[] niePalindrom = {4, 6, 8, 3, 6, 6, 4};//new char[];
+
+        CzyPalindrom(palindrom);
+        CzyPalindrom(niePalindrom);
 
         // Zadanie IX
         System.out.println("\nZadanie IX\n");
